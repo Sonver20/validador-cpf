a84ddb5b3c36503ef981f046c9c82eca69d09950
Sonver20 <eversonsouzafrias13@gmail.com>
2026-04-17 17:23:20 -0300

    Add files via upload

---
+++ validador_cpf.py
@@ -0,0 +1,49 @@
+recebe = input('Seu CPF: ')
+recebe = recebe.replace('.','').replace('-','')
+validacao = False
+
+if len(recebe) != 11:
+    print('CPF invalido')
+else:
+    if recebe.isnumeric():
+        cpf = []
+        validacao = True
+        for n in recebe:
+            a = int(n)
+            cpf.append(a)
+    else:
+        print('CPF invalido')
+
+d = 10
+soma = 0
+verifica = False
+if validacao:
+    for c in range(9):
+        calculo = cpf[c]*d
+        soma += calculo
+        d -= 1
+    resto = soma % 11
+    if resto >= 2:
+        resto = 11 - resto
+    else:
+        resto = 0
+    if resto != cpf[9]:
+        print(f'CPF invalido!')
+    else:
+        verifica = True
+    if verifica:
+        soma = 0
+        d = 11
+        for c in range(10):
+            calculo = cpf[c]*d
+            soma += calculo
+            d -= 1
+        resto = soma % 11
+        if resto >= 2:
+            resto = 11 - resto
+        else:
+            resto = 0
+        if resto != cpf[10]:
+            print('CPF invalido!')
+        else:
+            print(f'Seu CPF é valido!')
